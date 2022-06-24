a412c5b405c845a5d7024c511ff039c061540804
Danok <danokguillen2023@gmail.com>
2022-06-23 21:03:59 -0700

    VARIABLE

--- leccion_uno.py
+++ leccion_uno.py
@@ -2,15 +2,4 @@ x = 10
 print(x)
 print(type(x))
 
-miGrupoFavorito = "metalica"
-comentario = "The best rock band"
-#comentatios
-print("Mi grupo favorito es: ", miGrupoFavorito)
 
-variable = 2 > 4
-print(variable)
-
-if variable:
-    print('EL resultado fue verdadero')
-else:
-    print('El resultado es falso')
