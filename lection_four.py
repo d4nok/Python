e4628dc8d9c7a7ee4f67b78fe61482e6b87f9ffe
Danok <danokguillen2023@gmail.com>
2022-06-26 16:31:25 -0700

    string line 3

---
+++ lection_four.py
@@ -0,0 +1,10 @@
+#data input
+
+resultado = input("Type something:")
+print('welcome '+ resultado)
+
+numero1= int(input("Type the first number:"))
+numero2= int(input("Type the second number:"))
+resultado = numero1 + numero2
+print("Result of your sum:", resultado)
+#en esta parte de el codigo puedo ver como puedo puedo manipular la entrada de datos
